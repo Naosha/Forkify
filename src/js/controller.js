f2a667b3ecaba5d79f5add277086a0c7313722ff
Naosha <naomisharp@hotmail.co.uk>
2022-08-31 14:55:29 +0100

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,10 @@ const controlAddRecipe = async function (newRecipe) {
   // console.log(newRecipe);
 };
 
+const newFeature = function () {
+  console.log('Hi');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
